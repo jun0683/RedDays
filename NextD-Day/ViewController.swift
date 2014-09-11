09c17d93241d311488a9533dba0c36c2852cdc2b
국진 전 <kjjeon@gugjin-jeon-ui-MacBook-Pro.local>
2014-09-11 11:04:17 +0900

    Initial Commit

---
+++ NextD-Day/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NextD-Day
+//
+//  Created by 국진 전 on 2014. 9. 11..
+//  Copyright (c) 2014년 honggu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
---
+++ NextD-DayTests/NextD_DayTests.swift
@@ -0,0 +1,36 @@
+//
+//  NextD_DayTests.swift
+//  NextD-DayTests
+//
+//  Created by 국진 전 on 2014. 9. 11..
+//  Copyright (c) 2014년 honggu. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class NextD_DayTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
